bf9f6e743193708f2dbb0e23b99a9201cef79180
oscar mugambi <oscarmugambi@gmail.com>
2022-08-12 23:16:05 +0300

    optional chaining

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -22,8 +22,8 @@ const Users = () => {
     <div>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Logout</button>
-      <div>User name is</div>
-      <div>User email is</div>
+      <div>User name is {user?.name}</div>
+      <div>User email is {user?.email} </div>
     </div>
   )
 }
