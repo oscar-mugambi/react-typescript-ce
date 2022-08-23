e88c7bf7ca282b0f66d1e0faa90a0cd8a1d30797
oscar mugambi <oscarmugambi@gmail.com>
2022-08-23 22:05:13 +0300

    added additional prop

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,11 +1,13 @@
 type GreetProps = {
   name: string
+  messageCount: number
 }
 
 export const Greet = (props: GreetProps) => {
   return (
     <div>
       <h2>Lorem ipsum dolor sit. Sunt minima! {props.name}</h2>
+      <p>You have {props.messageCount} unread messages</p>
     </div>
   )
 }
